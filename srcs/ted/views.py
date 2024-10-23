14d4876e1d5a027bf734663b5d9680dedfe27585
tmaillar <tmaillar@student.42.fr>
2024-10-23 15:32:21 +0200

    index.html with first look menu

--- srcs/ted/views.py
+++ srcs/ted/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 
 def index(request):
 	context = {
-		"message": "Welcome on TED !",
+		"message": "PONG",
 		"number": 42,
 		"userList": ["Yabing", "Ludo", "David", "Thibaud"]
 		}
