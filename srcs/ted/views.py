ecbbcd3b0ccc770918570357d63c16922a6b6dc3
tmaillar <tmaillar@student.42.fr>
2024-10-23 13:03:22 +0200

    django backend init

---
+++ srcs/ted/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+	context = {
+		"message": "Welcome on TED !",
+		"number": 42,
+		"userList": ["Yabing", "Ludo", "David", "Thibaud"]
+		}
+	template = loader.get_template("ted/index.html")
+	return HttpResponse(template.render(context, request))
