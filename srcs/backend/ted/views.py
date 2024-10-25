ca030468534cd8700926bfd80432b07f0d90ab05
tmaillar <tmaillar@student.42.fr>
2024-10-25 15:15:25 +0200

    implement js test

--- srcs/backend/ted/views.py
+++ srcs/backend/ted/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import Message
 from django.template import loader
 
 def index(request):
@@ -10,3 +12,7 @@ def index(request):
 		}
 	template = loader.get_template("ted/index.html")
 	return HttpResponse(template.render(context, request))
+
+def get_messages(request):
+	messages = Message.objects.all().values('content')
+	return JsonResponse(list(messages), safe=False)
