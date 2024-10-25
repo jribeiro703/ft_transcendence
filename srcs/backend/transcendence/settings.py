d2e4945366eb8acf0ae66a03450feefe652470d5
ScrapingArt <scrapingart@protonmail.com>
2024-10-25 14:40:01 +0200

    updated Makefile: generating env file and certificates, removed secret key from django

--- srcs/backend/transcendence/settings.py
+++ srcs/backend/transcendence/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xx^+3vf464wwq2m0)!#wnz(s9&e&2k8s8*(bt0_919h^t^(2d#'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
