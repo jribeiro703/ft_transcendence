37c5f3e4de461f91f591c62f0878a2a769287341
ScrapingRat <scrapingrat@protonmail.com>
2024-10-28 14:59:17 +0100

    working https. Need to add an asgi/wsgi

--- srcs/backend/transcendence/settings.py
+++ srcs/backend/transcendence/settings.py
@@ -34,6 +34,15 @@ ALLOWED_HOSTS = [
 	'localhost',
 ]
 
+# HTTPS settings
+# SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# https://docs.djangoproject.com/en/5.1/topics/security/
 
 # Application definition
 
