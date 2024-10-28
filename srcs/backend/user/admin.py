bd88115ee1db05f57de9a6deae5d70e322deed13
yb <yabingc7@gmail.com>
2024-10-28 19:08:47 +0000

    create sign up api + improve user model + signup tests=ok

--- srcs/backend/user/admin.py
+++ srcs/backend/user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('username', 'email',)
+	search_fields = ('username', 'email',)
+	ordering = ('username',)
+	list_filter = ('email',)
+
+admin.site.register(User, UserAdmin)
