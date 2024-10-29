a922bd6072c7b7aaa271153ba94be477f767d945
yb <yabingc7@gmail.com>
2024-10-29 13:23:11 +0000

    add login, logout with JWT and alls models in admin.py

--- srcs/backend/user/views.py
+++ srcs/backend/user/views.py
@@ -1,9 +1,17 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import status
 from rest_framework import permissions
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .serializers import UserCreateSerializer
 
@@ -14,5 +22,63 @@ def user_index(request):
 
 class CreateUserView(CreateAPIView):
 	model = get_user_model()
-	permission_classes = [permissions.AllowAny]
+	permission_classes = [AllowAny]
 	serializer_class = UserCreateSerializer
+
+# class UserViewSet(viewsets.ModelViewSet):
+# 	queryset = User.objects.all()
+# 	serializer_class = UserSerializer
+# 	authentication_classes = [JWTAuthentication]
+    
+# 	def get_permissions(self):
+# 		if self.action == 'create':
+# 			permission_classes = [AllowAny]
+# 		elif self.action in ['update', 'destroy', 'retrieve']:
+# 			permission_classes = [IsAuthenticated, IsOwner | IsAdminUser]
+# 		else:
+# 			permission_classes = [IsAdminUser]
+# 		return [permission() for permission in permission_classes]
+
+
+class CookieTokenRefreshSerializer(TokenRefreshSerializer):
+    refresh = None
+
+    def validate(self, attrs):
+        attrs['refresh'] = self.context['request'].COOKIES.get('refresh_token')
+        if attrs['refresh']:
+            return super().validate(attrs)
+        else:
+            raise InvalidToken('No valid token found in cookie \'refresh_token\'')
+
+
+class CookieTokenObtainPairView(TokenObtainPairView):
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.data.get('refresh'):
+            cookie_max_age = 3600 * 24
+            response.set_cookie('refresh_token', response.data['refresh'], max_age=cookie_max_age, httponly=True)
+            del response.data['refresh']
+        return super().finalize_response(request, response, *args, **kwargs)
+
+
+class CookieTokenRefreshView(TokenRefreshView):
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.data.get('refresh'):
+            cookie_max_age = 3600 * 24
+            response.set_cookie('refresh_token', response.data['refresh'], max_age=cookie_max_age, httponly=True)
+            del response.data['refresh']
+        return super().finalize_response(request, response, *args, **kwargs)
+    serializer_class = CookieTokenRefreshSerializer
+
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.COOKIES.get('refresh_token')
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
