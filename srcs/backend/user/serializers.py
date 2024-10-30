b1a1804da2f3fba22df346ea60619c6effba8757
yb <yabingc7@gmail.com>
2024-10-30 09:59:26 +0100

    user rebased

--- srcs/backend/user/serializers.py
+++ srcs/backend/user/serializers.py
@@ -1,5 +1,6 @@
-from rest_framework import serializers
 from .models import User
+from django.core.mail import send_mail
+from rest_framework import serializers
 
 # class UserSerializer(serializers.ModelSerializer):
 # 	class Meta:
@@ -9,19 +10,6 @@ from .models import User
 # 		extra_kwargs = {
 #             'password': {'write_only': True}
 #         }
-
-# 	def create(self, validated_data):
-# 		password = validated_data.pop('password')
-# 		user = User(**validated_data)
-# 		user.set_password(password)
-# 		user.save()
-# 		return user
-
-# 	def update(self, instance, validated_data):
-# 		if 'password' in validated_data:
-# 			password = validated_data.pop('password')
-# 			instance.set_password(password)
-# 		return super(UserSerializer, self).update(instance, validated_data)
 	
 class UserCreateSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -30,6 +18,11 @@ class UserCreateSerializer(serializers.ModelSerializer):
 		extra_kwargs = {
             'password': {'write_only': True}
         }
+
+	def _send_activation_email(self, user):
+		subject = 'Activate Your Account'
+		message = f'Please activate your account by clicking the link: http://localhost:8000{reverse("activate", kwargs={"uidb64": urlsafe_base64_encode(force_bytes(user.pk)), "token": account_activation_token.make_token(user)})}'
+		send_mail(subject, message, 'from@example.com', [user.email])
 
 	def create(self, validated_data):
 		password = validated_data.pop('password')
