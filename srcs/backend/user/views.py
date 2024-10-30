706e11caf5b5662e7ecd555dc77d4d2a171c8e23
yb <yabingc7@gmail.com>
2024-10-30 09:59:26 +0100

    user rebased

--- srcs/backend/user/views.py
+++ srcs/backend/user/views.py
@@ -40,6 +40,9 @@ class CreateUserView(CreateAPIView):
 # 			permission_classes = [IsAdminUser]
 # 		return [permission() for permission in permission_classes]
 
+# @api_view(['POST'])
+# def login(request):
+     
 
 class CookieTokenRefreshSerializer(TokenRefreshSerializer):
     refresh = None
@@ -62,13 +65,13 @@ class CookieTokenObtainPairView(TokenObtainPairView):
 
 
 class CookieTokenRefreshView(TokenRefreshView):
+    serializer_class = CookieTokenRefreshSerializer
     def finalize_response(self, request, response, *args, **kwargs):
         if response.data.get('refresh'):
             cookie_max_age = 3600 * 24
             response.set_cookie('refresh_token', response.data['refresh'], max_age=cookie_max_age, httponly=True)
             del response.data['refresh']
         return super().finalize_response(request, response, *args, **kwargs)
-    serializer_class = CookieTokenRefreshSerializer
 
 
 class LogoutView(APIView):
