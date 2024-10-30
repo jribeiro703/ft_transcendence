e2048c37c5e6d53f6aaad23f39739f249cdf2150
yb <yabingc7@gmail.com>
2024-10-30 12:36:35 +0100

    implementing...of : user account activation mail and authentication 2FA

--- srcs/backend/user/views.py
+++ srcs/backend/user/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import get_user_model
+# from django.contrib.auth.tokens import default_token_generator
+# from django.utils.encoding import force_str
+# from django.utils.http import urlsafe_base64_decode
+# from django.shortcuts import get_object_or_404
 
 from rest_framework import status
-from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -9,11 +12,12 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.exceptions import InvalidToken
-from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .serializers import UserCreateSerializer
+# from django_otp.decorators import otp_required
+
+from .serializers import UserCreateSerializer, CookieTokenRefreshSerializer
+from .models import User
 
 @api_view(['GET'])
 def user_index(request):
@@ -26,6 +30,55 @@ class CreateUserView(CreateAPIView):
 	permission_classes = [AllowAny]
 	serializer_class = UserCreateSerializer
 
+# class ActivateAccountView(APIView):
+#     permission_classes = [AllowAny]
+
+#     def get(self, request, uidb64, token):
+#         try:
+#             uid = force_str(urlsafe_base64_decode(uidb64))
+#             user = get_object_or_404(User, pk=uid)
+#         except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+#             user = None
+
+#         if user is not None and default_token_generator.check_token(user, token):
+#             user.is_active = True
+#             user.save()
+#             return Response({"message": "Account activated successfully"}, status=status.HTTP_200_OK)
+#         else:
+#             return Response({"message": "Activation link is invalid"}, status=status.HTTP_400_BAD_REQUEST)
+        
+# @otp_required
+class CookieTokenObtainPairView(TokenObtainPairView):
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.data.get('refresh'):
+            cookie_max_age = 3600 * 24
+            response.set_cookie('refresh_token', response.data['refresh'], max_age=cookie_max_age, httponly=True)
+            del response.data['refresh']
+        return super().finalize_response(request, response, *args, **kwargs)
+
+class CookieTokenRefreshView(TokenRefreshView):
+    serializer_class = CookieTokenRefreshSerializer
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.data.get('refresh'):
+            cookie_max_age = 3600 * 24
+            response.set_cookie('refresh_token', response.data['refresh'], max_age=cookie_max_age, httponly=True)
+            del response.data['refresh']
+        return super().finalize_response(request, response, *args, **kwargs)
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.COOKIES.get('refresh_token')
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+				
 # class UserViewSet(viewsets.ModelViewSet):
 # 	queryset = User.objects.all()
 # 	serializer_class = UserSerializer
@@ -42,47 +95,3 @@ class CreateUserView(CreateAPIView):
 
 # @api_view(['POST'])
 # def login(request):
-     
-
-class CookieTokenRefreshSerializer(TokenRefreshSerializer):
-    refresh = None
-
-    def validate(self, attrs):
-        attrs['refresh'] = self.context['request'].COOKIES.get('refresh_token')
-        if attrs['refresh']:
-            return super().validate(attrs)
-        else:
-            raise InvalidToken('No valid token found in cookie \'refresh_token\'')
-
-
-class CookieTokenObtainPairView(TokenObtainPairView):
-    def finalize_response(self, request, response, *args, **kwargs):
-        if response.data.get('refresh'):
-            cookie_max_age = 3600 * 24
-            response.set_cookie('refresh_token', response.data['refresh'], max_age=cookie_max_age, httponly=True)
-            del response.data['refresh']
-        return super().finalize_response(request, response, *args, **kwargs)
-
-
-class CookieTokenRefreshView(TokenRefreshView):
-    serializer_class = CookieTokenRefreshSerializer
-    def finalize_response(self, request, response, *args, **kwargs):
-        if response.data.get('refresh'):
-            cookie_max_age = 3600 * 24
-            response.set_cookie('refresh_token', response.data['refresh'], max_age=cookie_max_age, httponly=True)
-            del response.data['refresh']
-        return super().finalize_response(request, response, *args, **kwargs)
-
-
-class LogoutView(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def post(self, request):
-        try:
-            refresh_token = request.COOKIES.get('refresh_token')
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-
-            return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
