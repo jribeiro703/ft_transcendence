99bfe457066f7e5fc6ffdb2dfe13425d555054ba
yb <yabingc7@gmail.com>
2024-10-30 15:12:55 +0100

    delete unused directory

--- srcs/backend/user/urls.py
+++ srcs/backend/user/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
 	path('', views.user_index),
 	path('signup/', views.CreateUserView.as_view(), name="signup"),
-	# path('signup/activate/<uidb64>/<token>/', views.ActivateAccountView.as_view(), name="signup-user-activate"),
+	path('signup/activate/<uidb64>/<token>/', views.ActivateAccountView.as_view(), name="signup-user-activate"),
 	path('login/', views.CookieTokenObtainPairView.as_view(), name="cookie_token_obtain_pair"),
     path('login/refresh/', views.CookieTokenRefreshView.as_view(), name="cookie_token_refresh"),
 	path('logout/', views.LogoutView.as_view(), name="logout")
