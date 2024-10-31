112793cce76d1ca0dfa6d4fd5f788a4e04627fcf
tmaillar <tmaillar@student.42.fr>
2024-10-31 10:50:31 +0100

    reorganize js, ai

---
+++ srcs/frontend/static/js/draw.js
@@ -0,0 +1,84 @@
+import gameVar from "./var.js";
+import { BALL_RADIUS, POWER_UP_SIZE } from "./const.js";
+
+export function initDraw()
+{
+	drawBall();
+	drawPaddles();
+	drawLines();
+}
+
+export function drawPowerUp()
+{
+	if (!gameVar.powerUpActive)
+	{
+		gameVar.ctx.beginPath();
+		gameVar.ctx.rect(gameVar.powerUpX, gameVar.powerUpY, POWER_UP_SIZE, POWER_UP_SIZE);
+		gameVar.ctx.fillStyle = "red";
+		gameVar.ctx.font = '20px Arial';
+		gameVar.ctx.fillText("Bonus", gameVar.powerUpX - 15 , gameVar.powerUpY + 40);
+		gameVar.ctx.fill();
+		gameVar.ctx.closePath();
+	}
+}
+
+export function drawPaddles()
+{
+	gameVar.ctx.beginPath();
+	gameVar.ctx.rect(0, gameVar.playerPaddleX, gameVar.playerPaddleWidth, gameVar.playerPaddleHeight);
+	gameVar.ctx.fillStyle = "#FF414D";
+	gameVar.ctx.fill();
+	gameVar.ctx.closePath();
+
+	gameVar.ctx.beginPath();
+	gameVar.ctx.rect(gameVar.canvasW - gameVar.aiPaddleWidth, gameVar.aiPaddleX, gameVar.aiPaddleWidth, gameVar.aiPaddleHeight);
+	gameVar.ctx.fillStyle = "#FF414D";
+	gameVar.ctx.fill();
+	gameVar.ctx.closePath();
+}
+
+export function drawLines() 
+{
+	gameVar.ctx.strokeStyle = 'white';
+	gameVar.ctx.lineWidth = 4;
+	gameVar.ctx.setLineDash([12, 12]);
+	gameVar.ctx.beginPath();
+	gameVar.ctx.moveTo(gameVar.canvasW / 2, 0);
+	gameVar.ctx.lineTo(gameVar.canvasW / 2, gameVar.canvasH);
+	gameVar.ctx.stroke();
+	gameVar.ctx.setLineDash([]); 
+
+	gameVar.ctx.lineWidth = 2;
+	gameVar.ctx.strokeRect(0, 0, gameVar.canvasW, gameVar.canvasH);
+
+	gameVar.ctx.lineWidth = 2;
+	gameVar.ctx.beginPath();
+	gameVar.ctx.moveTo(0, gameVar.canvasH / 2); 
+	gameVar.ctx.lineTo(gameVar.canvasW, gameVar.canvasH / 2);
+	gameVar.ctx.stroke();
+}
+
+export function initializeBall()
+{
+	if (gameVar.currenServer === 'player')
+	{
+		gameVar.x = gameVar.playerPaddleWidth + BALL_RADIUS; 
+		gameVar.y = gameVar.playerPaddleX + gameVar.playerPaddleHeight / 2;
+	}
+	else 
+	{
+		gameVar.x = gameVar.canvasw - gameVar.aiPaddleWidth - BALL_RADIUS;
+		gameVar.y = gameVar.aiPaddleX + gameVar.aiPaddleHeight / 2;
+	}
+	gameVar.dx = 0;
+	gameVar.dy = 0;
+}
+
+export function drawBall()
+{
+	gameVar.ctx.beginPath();
+	gameVar.ctx.arc(gameVar.x, gameVar.y, BALL_RADIUS, 0, Math.PI * 2);
+	gameVar.ctx.fillStyle = "#F8FF00";
+	gameVar.ctx.fill();
+	gameVar.ctx.closePath();
+}
