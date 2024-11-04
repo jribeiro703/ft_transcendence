fc55671d45403c22cbad4e8b9b97c8c8fe3a2d17
tmaillar <tmaillar@student.42.fr>
2024-11-04 09:59:46 +0100

    GameDev : add effect on rebond

--- srcs/static/js/reset.js
+++ srcs/static/js/reset.js
@@ -1,4 +1,5 @@
 import gameVar from "./var.js";
+import { BALL_RADIUS } from "./const.js";
 import { WIN_SCORE, GAP_SCORE,  } from "./const.js";
 import { initializeBall } from "./draw.js";
 import { aiServeBall } from "./ai.js";
@@ -66,4 +67,43 @@ export function resetBall(winner)
 		gameVar.aiServe = true;
 		aiServeBall();
 	}
+}
+
+export function updateBallPosition()
+{
+    gameVar.x += gameVar.dx;
+    gameVar.y += gameVar.dy;
+
+    if (gameVar.x + BALL_RADIUS > canvas.width || gameVar.x - BALL_RADIUS < 0)
+	{
+        gameVar.dx = -gameVar.dx;
+    }
+    if (gameVar.y - BALL_RADIUS < 0)
+	{
+        gameVar.dy = -gameVar.dy;
+    }
+    checkCollisionWithPaddle();
+    if (gameVar.y + BALL_RADIUS > canvas.height)
+	{
+        resetBall('ai');
+    }
+}
+
+export function checkCollisionWithPaddle()
+{
+    if (gameVar.y + BALL_RADIUS > gameVar.playerPaddleY &&
+        gameVar.x > gameVar.playerPaddleX &&
+        gameVar.x < gameVar.playerPaddleX + gameVar.playerPaddleWidth) 
+	{
+        gameVar.y = gameVar.playerPaddleY - BALL_RADIUS;
+        gameVar.dy = -gameVar.dy;
+    }
+
+    if (gameVar.y - BALL_RADIUS < gameVar.aiPaddleY + gameVar.aiPaddleHeight &&
+        gameVar.x > gameVar.aiPaddleX &&
+        gameVar.x < gameVar.aiPaddleX + gameVar.aiPaddleWidth)
+	{
+        gameVar.y = gameVar.aiPaddleY + gameVar.aiPaddleHeight + BALL_RADIUS;
+        gameVar.dy = -gameVar.dy;
+    }
 }
