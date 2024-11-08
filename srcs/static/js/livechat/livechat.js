2f3ed5448d22dde9290435e22a31a7d6377155d3
Haru Vulpes <Vulpes.Haru@gmail.com>
2024-11-08 04:15:30 +0100

    refont du home page

---
+++ srcs/static/js/livechat/livechat.js
@@ -0,0 +1,18 @@
+// const livechatBtn = document.getElementById('livechatBtn');
+// console.log(livechatBtn);
+//
+// livechatBtn.addEventListener('click', showLivechatView);
+//
+// function showLivechatView()
+// {
+//   if (livechatView.style.display === 'block')
+//   {
+//     livechatView.style.display = 'none';
+//   }
+//   else
+//     livechatView.style.display = 'block';
+// }
+//
+// faire un drawer en html
+// en position absolute avec top 0 et left viewport width
+//
