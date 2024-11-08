f7dfe907d4c3e68b580def0eb417af7a4b169331
ScrapingArt <scrapingart@protonmail.com>
2024-11-08 08:25:19 +0100

    room system

--- srcs/static/js/input.js
+++ srcs/static/js/input.js
@@ -1,46 +1,44 @@
 import gameVar from "./var.js";
 import { INIT_DX, INIT_DY, PADDLE_SPEED } from './const.js';
 
-export function manageMove()
+export function manageMove(isFirstPlayer)
 {
-	if (gameVar.playerUpPressed && gameVar.playerPaddleX > 0)
-	{
-		gameVar.playerPaddleX -= PADDLE_SPEED;
-	} 
-	else if (gameVar.playerDownPressed && gameVar.playerPaddleX < gameVar.canvasH - gameVar.playerPaddleHeight)
-	{
-		gameVar.playerPaddleX += PADDLE_SPEED;
-	}  
+	if (isFirstPlayer) {
+		if (gameVar.playerUpPressed && gameVar.playerPaddleX > 0) {
+			gameVar.playerPaddleX -= PADDLE_SPEED;
+		} else if (gameVar.playerDownPressed && gameVar.playerPaddleX < gameVar.canvasH - gameVar.playerPaddleHeight) {
+			gameVar.playerPaddleX += PADDLE_SPEED;
+		}
+	} else {
+		if (gameVar.playerUpPressed && gameVar.aiPaddleX > 0) {
+			gameVar.aiPaddleX -= PADDLE_SPEED;
+		} else if (gameVar.playerDownPressed && gameVar.aiPaddleX < gameVar.canvasH - gameVar.aiPaddleHeight) {
+			gameVar.aiPaddleX += PADDLE_SPEED;
+		}
+	}
 }
 
-export function keyDownHandler(e)
+export function keyDownHandler(e, isFirstPlayer)
 {
-	if (e.key == "ArrowUp")
-	{
+	if (e.key == "ArrowUp") {
 		gameVar.playerUpPressed = true;
-	}
-	else if (e.key == "ArrowDown") 
-	{
+	} else if (e.key == "ArrowDown") {
 		gameVar.playerDownPressed = true;
 	}
 }
-	
-export function keyUpHandler(e)
+
+export function keyUpHandler(e, isFirstPlayer)
 {
-	if (e.key == "ArrowUp")
-	{
+	if (e.key == "ArrowUp") {
 		gameVar.playerUpPressed = false;
-	} 
-	else if (e.key == "ArrowDown")
-	{
+	} else if (e.key == "ArrowDown") {
 		gameVar.playerDownPressed = false;
 	}
 }
 
 export function startBall(e)
 {
-	if (e.code == "Space" && !gameVar.matchOver && !gameVar.aiServe && !gameVar.gameStart)
-	{
+	if (e.code == "Space" && !gameVar.matchOver && !gameVar.aiServe && !gameVar.gameStart) {
 		gameVar.gameStart = true;
 		gameVar.dx = INIT_DX;
 		gameVar.dy = (Math.random() < 0.5 ? INIT_DY : -INIT_DY);
