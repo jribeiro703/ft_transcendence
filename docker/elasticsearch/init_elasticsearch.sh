7e5c24508835427324da35985e092995888d6984
ScrapingRat <scrapingrat@protonmail.com>
2024-11-11 17:18:19 +0100

    password protection

--- docker/elasticsearch/init_elasticsearch.sh
+++ docker/elasticsearch/init_elasticsearch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Wait for Elasticsearch to start
-until curl -s http://localhost:9200/_cluster/health | grep -q '"status":"green"\|"status":"yellow"'; do
+until curl -s -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" --cacert /usr/share/elasticsearch/config/certs/elasticsearch.crt --key /usr/share/elasticsearch/config/certs/elasticsearch.key --cert /usr/share/elasticsearch/config/certs/elasticsearch.crt https://localhost:9200/_cluster/health | jq -e '.status == "green" or .status == "yellow"' > /dev/null; do
   echo "Waiting for Elasticsearch to start..."
   sleep 5
 done
 
 # Apply ILM policy
-curl -X PUT "http://localhost:9200/_ilm/policy/logs_policy" -H 'Content-Type: application/json' -d '{
+curl -X PUT -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" --cacert /usr/share/elasticsearch/config/certs/elasticsearch.crt --key /usr/share/elasticsearch/config/certs/elasticsearch.key --cert /usr/share/elasticsearch/config/certs/elasticsearch.crt "https://localhost:9200/_ilm/policy/logs_policy" -H 'Content-Type: application/json' -d '{
   "policy": {
     "phases": {
       "hot": {
@@ -29,7 +29,7 @@ curl -X PUT "http://localhost:9200/_ilm/policy/logs_policy" -H 'Content-Type: ap
 }'
 
 # Create snapshot repository
-curl -X PUT "http://localhost:9200/_snapshot/logs_backup" -H 'Content-Type: application/json' -d '{
+curl -X PUT -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" --cacert /usr/share/elasticsearch/config/certs/elasticsearch.crt --key /usr/share/elasticsearch/config/certs/elasticsearch.key --cert /usr/share/elasticsearch/config/certs/elasticsearch.crt "https://localhost:9200/_snapshot/logs_backup" -H 'Content-Type: application/json' -d '{
   "type": "fs",
   "settings": {
     "location": "/usr/share/elasticsearch/snapshots"
@@ -37,7 +37,7 @@ curl -X PUT "http://localhost:9200/_snapshot/logs_backup" -H 'Content-Type: appl
 }'
 
 # Apply ILM policy to index template
-curl -X PUT "http://localhost:9200/_template/logs_template" -H 'Content-Type: application/json' -d '{
+curl -X PUT -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" --cacert /usr/share/elasticsearch/config/certs/elasticsearch.crt --key /usr/share/elasticsearch/config/certs/elasticsearch.key --cert /usr/share/elasticsearch/config/certs/elasticsearch.crt "https://localhost:9200/_template/logs_template" -H 'Content-Type: application/json' -d '{
   "index_patterns": ["logstash-*"],
   "settings": {
     "index.lifecycle.name": "logs_policy",
