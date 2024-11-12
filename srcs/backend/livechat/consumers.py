5994f0a7e1be090fae4daae68a15318e27fbd341
Shiro Vulpes <Vulpes.Shiro@gmail.com>
2024-11-12 21:44:48 +0100

    ok

--- srcs/backend/livechat/consumers.py
+++ srcs/backend/livechat/consumers.py
@@ -1,17 +1,38 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.roomGroupName = "group_chat"
+        await self.channel_layer.group_add(
+            self.roomGroupName,
+            self.channel_name
+        )
+        await self.accept()
 
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.roomGroupName,
+            self.channel_layer
+        )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        username = text_data_json["username"]
+        time = text_data_json["time"]
+        await self.channel_layer.group_send(
+            self.roomGroupName, {
+                "type": "sendMessage",
+                "message": message,
+                "username": username,
+                "time": time
+            })
 
-        self.send(text_data=json.dumps({"message": message}))
+    async def sendMessage(self, event):
+        message = event["message"]
+        username = event["username"]
+        time = event["time"]
+        await self.send(text_data=json.dumps({"message": message, "username": username, "time": time}))
--- srcs/backend/livechat/views.py
+++ srcs/backend/livechat/views.py
@@ -7,7 +7,7 @@ def livechat_index(request):
     data = {"message": "Hello, world from livechat app !"}
     return Response(data, status=status.HTTP_200_OK)
 
-@api_view(['GET'])
-def room(request, livechat):
-    data = {"livechat": livechat}
-    return Response(data, status=status.HTTP_200_OK)
+# @api_view(['GET'])
+# def room(request, livechat):
+#     data = {"livechat": livechat}
+#     return Response(data, status=status.HTTP_200_OK)
