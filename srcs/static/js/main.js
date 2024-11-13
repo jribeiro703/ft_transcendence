34e70c65c9a3360f26d9f055fb43e0644eaec14e
yb <yabingc7@gmail.com>
2024-11-13 10:44:30 +0100

    update branch user with last main version

---
+++ srcs/static/js/main.js
@@ -0,0 +1,25 @@
+import gameVar from './var.js';
+import { updatePowerUpSelection } from './powerUp.js';
+import { updateLevelSelection } from './gameMode.js';
+import { initGameVar, initEventListener } from './init.js';
+import { manageAi } from './ai.js';
+
+document.addEventListener('DOMContentLoaded', function() 
+{
+	const link = document.createElement('link');
+	link.rel = 'stylesheet';
+	link.href = 'static/css/style.css';
+	link.type = 'text/css';
+
+	document.head.appendChild(link);
+	var canvas = document.getElementById('myCanvas');
+	gameVar.ctx = canvas.getContext('2d');
+	canvas.width = gameVar.canvasW;
+	canvas.height = gameVar.canvasH;
+
+	initGameVar();
+	initEventListener();
+
+	updateLevelSelection();
+	updatePowerUpSelection(false);
+});
