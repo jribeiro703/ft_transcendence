c90edca13a5a70655d488949934f107d71c7c560
lannur-s <lathaannur@gmail.com>
2024-11-14 13:27:50 +0100

    Added new page for the Tournament button

---
+++ srcs/static/js/tournamentView.js
@@ -0,0 +1,16 @@
+import gameVar from "./var.js";
+
+export function showTournamentView()
+{
+	gameplayView.style.display = 'none';
+	quickGameBtn.style.display = 'none';
+	startGameBtn.style.display = 'none';
+	tournamentGameBtn.style.display = 'none'
+
+	gameView.style.display = 'none';
+	rematchBtn.style.display = 'none';
+	quitGameBtn.style.display = 'none';	
+
+	tournamentView.style.display = 'block';
+	startTournamentBtn.style.display = 'block';
+}
