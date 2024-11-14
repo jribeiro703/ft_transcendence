fd8c9e72623d8fce9780d814819b860d3ee441aa
lannur-s <lathaannur@gmail.com>
2024-11-14 19:54:26 +0100

    trying to receive list of participants via websocket

--- srcs/backend/tournament/routing.py
+++ srcs/backend/tournament/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/basic/', consumers.BasicConsumer.as_asgi()),
+	# re_path(r'ws/basic/', consumers.BasicConsumer.as_asgi()),
+	re_path(r'ws/tournament/', consumers.TournamentConsumer.as_asgi()),
 ]
