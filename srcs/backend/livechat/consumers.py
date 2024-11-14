00b082a747d561fd9461e2c2a7bebd1a891078cf
Haru Vulpes <Vulpes.Haru@gmail.com>
2024-11-14 08:02:38 +0100

    ok

--- srcs/backend/livechat/consumers.py
+++ srcs/backend/livechat/consumers.py
@@ -1,39 +1,72 @@
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from .models import ChatMemberModel, ChatMessageModel
+
+import time
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.room_name = 'livechat'
-        self.room_group_name = 'chat_%s' % self.room_name
 
-        await self.channel_layer.group_add(
+class ChatConsumer(WebsocketConsumer):
+
+    def connect(self):
+
+        self.user = self.scope["user"]
+        if not self.user.is_authenticated:
+            return
+
+        self.channel_id: int = int(self.scope['url_route']['kwargs']['chat_id'])
+
+        if not ChatMemberModel.objects.filter(member_id=self.user.pk, channel_id=self.channel_id).exists():
+            return
+
+        if self.channel_layer is None:
+            return
+
+        self.room_group_name = f'chat{self.channel_id}'
+
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
+        self.accept()
 
-        await self.accept()
+    def receive(self, text_data=None, bytes_data=None):
+        if text_data is None:
+            return
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        user = self.scope["user"]
+        if (user.is_anonymous or not user.is_authenticated):
+            return
 
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        text_data_json: dict = json.loads(text_data)
 
-        await self.channel_layer.group_send(
+        message = text_data_json.get('message')
+        if message is None:
+            return
+
+        message_time: int = int(time.time() * 1000)
+
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'author_id': user.pk,
+                'content': message,
+                'time': message_time,
             }
         )
 
-    async def chat_message(self, event):
-        message = event['message']
+        ChatMessageModel(
+            channel_id=self.channel_id,
+            author_id=user.pk,
+            content=message,
+            time=message_time
+        ).save()
 
-        await self.send(text_data=json.dumps({
-            'message': message
+    def chat_message(self, event):
+        self.send(text_data=json.dumps({
+            'type': 'chat',
+            'author_id': event['author_id'],
+            'content': event['content'],
+            'time': event['time'],
         }))
