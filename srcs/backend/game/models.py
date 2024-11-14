de832deeb70c5341b3b2ecc43ad55cf69517e8a3
yb <yabingc7@gmail.com>
2024-11-14 10:48:24 +0100

    Commented out parts that have been developed but not yet thoroughly tested

--- srcs/backend/game/models.py
+++ srcs/backend/game/models.py
@@ -3,12 +3,12 @@ from user.models import User
 from django.utils.translation import gettext_lazy as _
 
 class Game(models.Model):
-	created_at = models.DateTimeField(auto_now_add=True) # match will be created at frontend
+	created_at = models.DateTimeField(auto_now_add=False) # match will be created at frontend
 	player_one = models.ForeignKey(User, related_name='game_as_player_one', on_delete=models.SET_NULL, null=True)
 	player_two = models.ForeignKey(User, related_name='game_as_player_two', on_delete=models.SET_NULL, null=True)
+	winner = models.ForeignKey(User, related_name='game_as_winner', on_delete=models.SET_NULL, null=True)
 	score_one = models.IntegerField(default=0)
 	score_two = models.IntegerField(default=0)
-	winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
 	# related_name = tournaments
 	# game_instance.tournaments.all() to have access to alls tournaments
--- srcs/backend/game/urls.py
+++ srcs/backend/game/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
 	path('', views.game_index)
-	path('create/', views.RegisterGameView.as_view(), name="game_create"),
+	# path('create/', views.GameCreateView.as_view(), name="game_create"),
 ]
