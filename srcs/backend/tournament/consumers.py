e501254ddf29d217538c8b4f5b56e1bdd13f1a43
lannur-s <lathaannur@gmail.com>
2024-11-14 19:54:26 +0100

    trying to receive list of participants via websocket

--- srcs/backend/tournament/consumers.py
+++ srcs/backend/tournament/consumers.py
@@ -1,15 +1,18 @@
-# consumers.py
+# tournament/consumers.py
 from channels.generic.websocket import WebsocketConsumer
 import json
 
-class BasicConsumer(WebsocketConsumer):
+class TournamentConsumer(WebsocketConsumer):
 	def connect(self):
 		# Step 1: Accept the WebSocket connection
 		self.accept()
 		
+		all_users = self.get_all_users()
+
 		# Step 2: Send a welcome message to the client
 		self.send(text_data=json.dumps({
-			'message': 'Welcome to the WebSocket connection!'
+			'message': 'Welcome to the WebSocket connection!',
+			'participants': ['test1', 'test2']
 		}))
 
 	def disconnect(self, close_code):
