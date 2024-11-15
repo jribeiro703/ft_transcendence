2e1f4794fab804ab2f855fee366c2c2cd3cd6bed
yb <yabingc7@gmail.com>
2024-11-15 11:46:28 +0100

    game/create/ url done

--- srcs/backend/game/serializers.py
+++ srcs/backend/game/serializers.py
@@ -1,21 +1,19 @@
-# from rest_framework import serializers, exceptions
-# from .models import Game
-# from user.models import User
+from rest_framework import serializers
+from .models import Game
+from user.models import User
 
-# class GameCreateSerializer(serializers.ModelSerializer):
-# 	class Meta:
-# 		model = Game
-# 		fields = ('id', 'created_at', 'player_one', 'player_two', 'score_one', 'score_two', 'winner')
+class GameCreateSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Game
+		fields = ('id', 'created_at', 'player_one', 'player_two', 'score_one', 'score_two', 'winner')
 
-# 	def create(self, validated_data):
-# 		player_one = User.objects.get(id=validated_data['player_one_username'])
-# 		player_two = User.objects.get(id=validated_data['player_two_username'])
-# 		winner = User.objects.get(id=validated_data['winner_username'])
-# 		game = Game.objects.create(
-# 			player_one=player_one,
-# 			player_two=player_two,
-# 			score_one=validated_data['score_one'],
-# 			score_two=validated_data['score_two'],
-# 			winner=winner
-#     	)
-# 		return game
+	def create(self, validated_data):
+		game = Game.objects.create(
+			player_one=validated_data['player_one'],
+			player_two=validated_data['player_two'],
+			score_one=validated_data['score_one'],
+			score_two=validated_data['score_two'],
+			created_at=validated_data['created_at'],
+			winner=validated_data['winner']
+    	)
+		return game
