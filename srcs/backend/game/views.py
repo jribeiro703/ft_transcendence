c2c069411b83a9295a948ebe5f7971655fb9685a
yb <yabingc7@gmail.com>
2024-11-15 11:50:35 +0100

    add permission class to game view

--- srcs/backend/game/views.py
+++ srcs/backend/game/views.py
@@ -2,9 +2,12 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.generics import CreateAPIView
 from .serializers import GameCreateSerializer
 from .models import Game
+from user.permissions import IsOwner
 
 @api_view(['GET'])
 def game_index(request):
@@ -14,5 +17,5 @@ def game_index(request):
 class GameCreateView(CreateAPIView):
 	queryset = Game.objects.all()
 	serializer_class = GameCreateSerializer
-	authentication_classes = []
-	permission_classes = []
+	authentication_classes = [JWTAuthentication]
+	permission_classes = [IsAuthenticated, IsOwner]
