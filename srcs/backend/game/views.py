2f258faadb89c3ca60700aed748e03b608c6b3ee
yb <yabingc7@gmail.com>
2024-11-15 11:46:28 +0100

    game/create/ url done

--- srcs/backend/game/views.py
+++ srcs/backend/game/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
-
+from .serializers import GameCreateSerializer
 from .models import Game
 
 @api_view(['GET'])
@@ -11,5 +11,8 @@ def game_index(request):
 	data = {"message": "Hello, world from game app !"}
 	return Response(data, status=status.HTTP_200_OK)
 
-# class RegisterGameView(CreateAPIView):
-# 	queryset = Game.objects.all()
+class GameCreateView(CreateAPIView):
+	queryset = Game.objects.all()
+	serializer_class = GameCreateSerializer
+	authentication_classes = []
+	permission_classes = []
