dbdcc99e1dffa018cb5d3574df522a6557858134
lannur-s <lathaannur@gmail.com>
2024-11-20 12:35:14 +0100

    changed the UIDs; todo

--- srcs/backend/game/models.py
+++ srcs/backend/game/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from user.models import User
 
 class Game(models.Model):
-	id = models.UUIDField(primary_key=True, default=models.UUIDField, editable=False) # PK
+	# id = models.UUIDField(primary_key=True, default=models.uuid4, editable=False) # PK
 	player_one = models.ForeignKey(User, related_name='game_as_player_one', on_delete=models.SET_NULL, null=True) # FK to User
 	player_two = models.ForeignKey(User, related_name='game_as_player_two', on_delete=models.SET_NULL, null=True) # FK to User
 	winner = models.ForeignKey(User, related_name='game_as_winner', on_delete=models.SET_NULL, null=True) # FK to User
@@ -11,7 +11,7 @@ class Game(models.Model):
 	score_two = models.IntegerField(default=0)
 	max_score = models.IntegerField(default=10)
 	
-	tournament_id = models.ForeignKey(Tournament, related_name='games', on_delete=models.CASCADE, null=True) # FK to Tournament
+	# tournament_id = models.ForeignKey(Tournament, related_name='games', on_delete=models.CASCADE, null=True) # FK to Tournament
 	
 	created_at = models.DateTimeField(auto_now_add=False) # match will be created at frontend
 	start_time = models.DateTimeField(null=True, blank=True)
@@ -21,7 +21,7 @@ class Game(models.Model):
 		return _(f"Match started at {self.created_at}")
 
 class GamePlayer(models.Model):
-	id = models.UUIDField(primary_key=True, default=models.UUIDField, editable=False) # PK
+	# id = models.UUIDField(primary_key=True, default=models.uuid4, editable=False) # PK
 	game_id = models.ForeignKey(Game, related_name='game_players', on_delete=models.CASCADE)  # FK to Game
 	user_id = models.ForeignKey(User, related_name='game_participations', on_delete=models.CASCADE)  # FK to User
 	display_name = models.CharField(max_length=50, blank=True, null=True)
