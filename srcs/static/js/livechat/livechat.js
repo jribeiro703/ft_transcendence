c5d20b3f0e4572ca4874ea793ba6ea579e286d75
ScrapingArt <scrapingart@protonmail.com>
2024-11-20 17:52:09 +0100

    something something

--- srcs/static/js/livechat/livechat.js
+++ srcs/static/js/livechat/livechat.js
@@ -11,6 +11,13 @@ chatSocket.onclose = function(e) {
     console.error('Chat socket closed unexpectedly');
 };
 
+document.querySelector('#chat-message-input').focus();
+document.querySelector('#chat-message-input').onkeyup = function(e) {
+    if (e.keyCode === 13) {  // Enter key
+        document.querySelector('#chat-message-submit').click();
+    }
+};
+
 document.querySelector('#chat-message-submit').onclick = function(e) {
     const messageInputDom = document.querySelector('#chat-message-input');
     const message = messageInputDom.value;
