6a31e8b853bee00df82eeab1354516aee5711085
ScrapingRat <scrapingrat@protonmail.com>
2024-11-20 19:10:20 +0100

    working livechat

--- srcs/backend/livechat/consumers.py
+++ srcs/backend/livechat/consumers.py
@@ -2,6 +2,9 @@ import json
 import random
 import string
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.apps import apps
+from asgiref.sync import sync_to_async
+from datetime import datetime
 
 def generate_nickname():
     return ''.join(random.choices(string.ascii_letters + string.digits, k=5))
@@ -20,6 +23,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
+        # Send the last 100 messages to the client
+        messages = await self.get_last_100_messages()
+        for message in messages:
+            await self.send(text_data=json.dumps({
+                'message': message.content,
+                'client_id': message.nickname,
+                'timestamp': message.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+            }))
+
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
@@ -32,22 +44,38 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = text_data_json['message']
         client_id = self.nickname
 
+        # Save message to the database
+        await self.save_message(client_id, message)
+
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                'client_id': client_id
+                'client_id': client_id,
+                'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
         client_id = event['client_id']
+        timestamp = event['timestamp']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message,
-            'client_id': client_id
+            'client_id': client_id,
+            'timestamp': timestamp
         }))
+
+    @sync_to_async
+    def save_message(self, nickname, content):
+        Message = apps.get_model('livechat', 'Message')
+        Message.objects.create(nickname=nickname, content=content)
+
+    @sync_to_async
+    def get_last_100_messages(self):
+        Message = apps.get_model('livechat', 'Message')
+        return Message.objects.order_by('-timestamp')[:100][::-1]
--- srcs/backend/livechat/models.py
+++ srcs/backend/livechat/models.py
@@ -1,41 +1,9 @@
-from django.db.models import BigIntegerField, Model, IntegerField, ForeignKey, CharField, CASCADE
-from django.db.models.signals import post_delete
-from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.db import models
 
+class Message(models.Model):
+    nickname = models.CharField(max_length=5)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
-class ChatChannelModel(Model):
-    def create(self, members: [User]):
-        self.save()
-        for member in members:
-            ChatMemberModel(channel=self, member=member).save()
-        return self
-
-    def get_members(self):
-        return [member_channel.member for member_channel in ChatMemberModel.objects.filter(channel=self)]
-
-
-class ChatMemberModel(Model):
-    member = ForeignKey(User, on_delete=CASCADE)
-    channel = ForeignKey(ChatChannelModel, on_delete=CASCADE)
-
-
-@receiver(post_delete, sender=ChatMemberModel)
-def delete_channel_when_member_deleted(sender, instance, **kwargs):
-    print(sender, instance)
-
-
-class ChatMessageModel(Model):
-    channel = ForeignKey(ChatChannelModel, on_delete=CASCADE)
-    author = ForeignKey(User, on_delete=CASCADE)
-    content = CharField(max_length=1024)
-    time = BigIntegerField(primary_key=False)
-
-
-class AskModel(Model):
-    asker_id = IntegerField(primary_key=False)
-    asked_id = IntegerField(primary_key=False)
-
-    # return if the asker ask the asked to play a game
-    def is_asked(self, asker_id, asked_id):
-        return AskModel.objects.filter(asker_id=asker_id, asked_id=asked_id).first() is not None
+    def __str__(self):
+        return f'{self.nickname}: {self.content}'
