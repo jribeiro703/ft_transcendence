fdc6ca44ecd65febd4370214fdaecb1e5abc6d9a
lannur-s <lathaannur@gmail.com>
2024-11-22 12:27:18 +0100

    Game, GPlayer, Tournament models updated with migrations

--- srcs/backend/game/models.py
+++ srcs/backend/game/models.py
@@ -1,33 +1,45 @@
 from django.db import models
-from user.models import User
 # ensures that _ is defined as a shortcut for the gettext function for internationalization (i18n)
+# helps for lazy references to avoid circular dependencies
 from django.utils.translation import gettext as _ 
 
 class Game(models.Model):
-	# id = models.UUIDField(primary_key=True, default=models.uuid4, editable=False) # PK
-	player_one = models.ForeignKey(User, related_name='game_as_player_one', on_delete=models.SET_NULL, null=True) # FK to User
-	player_two = models.ForeignKey(User, related_name='game_as_player_two', on_delete=models.SET_NULL, null=True) # FK to User
-	winner = models.ForeignKey(User, related_name='game_as_winner', on_delete=models.SET_NULL, null=True) # FK to User
+	player_one = models.ForeignKey(
+		'user.User', related_name='game_as_player_one', on_delete=models.SET_NULL, null=True
+	) # FK to User
+	player_two = models.ForeignKey(
+		'user.User', related_name='game_as_player_two', on_delete=models.SET_NULL, null=True
+	) # FK to User
+	winner = models.ForeignKey(
+		'user.User', related_name='game_as_winner', on_delete=models.SET_NULL, null=True
+	) # FK to User
 	
-	score_one = models.IntegerField(default=0)
-	score_two = models.IntegerField(default=0)
-	max_score = models.IntegerField(default=10)
+	score_one = models.PositiveIntegerField(default=0)
+	score_two = models.PositiveIntegerField(default=0)
+	max_score = models.PositiveIntegerField(default=10)
 	
-	# tournament_id = models.ForeignKey(Tournament, related_name='games', on_delete=models.CASCADE, null=True) # FK to Tournament
+	tournament = models.ForeignKey(
+		'tournament.Tournament', related_name='tournament_games', on_delete=models.CASCADE, null=True
+	) # FK to Tournament
 	
-	created_at = models.DateTimeField(auto_now_add=False) # match will be created at frontend
+	created_at = models.DateTimeField(auto_now_add=True)  # Automatically set when created
 	start_time = models.DateTimeField(null=True, blank=True)
 	end_time = models.DateTimeField(null=True, blank=True)
 
 	def __str__(self):
-		return (f"Match started at {self.created_at}")
+		return f"Game {self.id} between {self.player_one} and {self.player_two}"
 
 class GamePlayer(models.Model):
-	# id = models.UUIDField(primary_key=True, default=models.uuid4, editable=False) # PK
-	game_id = models.ForeignKey(Game, related_name='game_players', on_delete=models.CASCADE)  # FK to Game
-	user_id = models.ForeignKey(User, related_name='game_participations', on_delete=models.CASCADE)  # FK to User
-	display_name = models.CharField(max_length=50, blank=True, null=True)
+	game = models.ForeignKey(
+		'game.Game', related_name='game_players', on_delete=models.CASCADE
+	)  # FK to Game
+	user = models.ForeignKey(
+		'user.User', related_name='game_participations', on_delete=models.CASCADE
+	)  # FK to User
+	display_name = models.CharField(
+		max_length=50, blank=True, null=True
+	)
 	is_winner  = models.BooleanField(default=False)
 
 	def __str__(self):
-		return (f"Player {self.user.username} in Game {self.game.id}")
+		return f"GamePlayer {self.user_id.username} in Game {self.game_id.id}"
--- srcs/backend/tournament/models.py
+++ srcs/backend/tournament/models.py
@@ -1,20 +1,31 @@
 from django.db import models
-from game.models import Game
-from user.models import User
-from django.utils.translation import gettext_lazy as _
+# ensures that _ is defined as a shortcut for the gettext function for internationalization (i18n)
+# helps for lazy references to avoid circular dependencies
+from django.utils.translation import gettext as _ 
 
 class Tournament(models.Model):
-	# uuid = models.UUIDField(primary_key=True, default=models.uuid4, editable=False)
-	created_by = models.ForeignKey(User, related_name='created_tournaments', on_delete=models.CASCADE)
+	created_by = models.ForeignKey(
+		'user.User', related_name='created_tournaments', on_delete=models.CASCADE
+	)
 	created_at = models.DateTimeField(auto_now_add=True)
 	start_date = models.DateTimeField(null=True, blank=True)  # Start date of the tournament
 	end_date = models.DateTimeField(null=True, blank=True)  # End date of the tournament
-	status = models.CharField(max_length=20, default="upcoming")  # E.g., upcoming, ongoing, finished
-	players = models.ManyToManyField(User, related_name='tournaments')  # Players participating
-	winner = models.ForeignKey(User, related_name='tournaments_won', on_delete=models.SET_NULL, null=True, blank=True)  # Optional winner
-	games = models.ManyToManyField(Game, related_name='tournaments')  # Games in the tournament
+	status = models.CharField(
+		max_length=20, default="upcoming"
+	)  # Options: upcoming, ongoing, finished
+	players = models.ManyToManyField(
+		'user.User', related_name='tournaments'
+	)  # Players participating
+	winner = models.ForeignKey(
+		'user.User', related_name='tournaments_won', on_delete=models.SET_NULL, null=True, blank=True
+	)  # Optional winner
+	games = models.ManyToManyField(
+		'game.Game', related_name='tournament_matchups'
+	)  # Games in the tournament
 	max_score = models.IntegerField(default=100)  # Tournament win threshold
-	matches = models.TextField(blank=True, null=True)  # Optional: serialized or textual match summary
+	matches = models.TextField(
+		blank=True, null=True
+	)  # Optional: serialized or textual match summary
 
 	def __str__(self):
 		return _(f"Tournament created by {self.created_by.username} with {self.players.count()} players")
