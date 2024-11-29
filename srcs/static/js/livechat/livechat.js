1c30da1a971ca220fee3d3a06c4c0ecd26e8c944
dlacuey <lacuey.david@gmail.com>
2024-11-29 03:21:42 +0100

    ok

--- srcs/static/js/livechat/livechat.js
+++ srcs/static/js/livechat/livechat.js
@@ -181,19 +181,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // slide le livechat
 
+
+// petite icone pour le livechat
+const chatIconDots = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-chat-left-dots-fill" viewBox="0 0 16 16">
+    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm5 4a1 1 0 1 0-2 0 1 1 0 0 0 2 0m4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+  </svg>
+`;
+
+const chatIconFill = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#2f2f2f" class="bi bi-chat-left-fill" viewBox="0 0 16 16">
+    <path d="M2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+  </svg>
+`;
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('[data-chat-icon]').innerHTML = chatIconFill;
+});
+
 function slideDiv() {
   const slidingDiv = document.getElementById('livechat');
+  const chatIcon = document.querySelector('[data-chat-icon]');
+
   slidingDiv.classList.toggle('visible');
   slidingDiv.classList.toggle('hide-children');
   slidingDiv.classList.toggle('disable-neon');
+
+  chatIcon.innerHTML = slidingDiv.classList.contains('visible') ? chatIconDots : chatIconFill;
 }
 
 document.getElementById('liveChatBtn').addEventListener('click', slideDiv);
-// end slide le livechat
-
-// svg du livechat
-document.querySelector('[data-chat-icon]').insertAdjacentHTML('beforeend', `
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-dots-fill" viewBox="0 0 16 16">
-    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm5 4a1 1 0 1 0-2 0 1 1 0 0 0 2 0m4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
-  </svg>
-`);
+document.querySelector('[data-chat-icon]').addEventListener('click', slideDiv);
