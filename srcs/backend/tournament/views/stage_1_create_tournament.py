ca6417ca05d480c16c179f244d915ea03931e431
lannur-s <lathaannur@gmail.com>
2024-12-03 14:32:47 +0100

    Autocreation of Tournament Name

--- srcs/backend/tournament/views/stage_1_create_tournament.py
+++ srcs/backend/tournament/views/stage_1_create_tournament.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.apps import apps
+from django.db import IntegrityError
 from rest_framework.permissions import IsAuthenticated
 
 from user.models import User
@@ -32,6 +33,9 @@ class CreateTournamentView(APIView):
 	"""
 	def post(self, request):
 		try:
+			# Log the incoming data
+			print('Incoming data:', request.data)
+
 			# Validate the incoming data
 			serializer = TournamentSerializer(data=request.data)
 			if serializer.is_valid():
@@ -41,17 +45,21 @@ class CreateTournamentView(APIView):
 				# Return the tournament ID and status
 				return Response({
 					"tournament_id": tournament.id,
-					"status": tournament.status
+					"status": tournament.status,
+					"name": tournament.name
 				}, status=status.HTTP_201_CREATED)
 			# Return validation errors
+			print('Validation errors:', serializer.errors)  # Log the validation errors
 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+		except IntegrityError as e:
+			# Handle unique constraint violation
+			print(f"Error creating tournament: {e}")
+			return Response({"error": "Tournament name must be unique."}, status=status.HTTP_400_BAD_REQUEST)
 		except Exception as e:
 			# Log the error for debugging
 			print(f"Error creating tournament: {e}")
 			# Return a generic error response
 			return Response({"error": "Internal server error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
 # ❶ TOURNAMENT CREATTION STAGE - Pre-register Players for a tournament
 class PreRegisterPlayersView(APIView):
 	"""
