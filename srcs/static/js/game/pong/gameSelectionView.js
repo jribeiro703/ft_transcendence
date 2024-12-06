4aa18095a84228753a002399d66198828f05e252
dlacuey <lacuey.david@gmail.com>
2024-12-06 10:41:44 +0100

    en cours de changement de la settingsView

--- srcs/static/js/game/pong/gameSelectionView.js
+++ srcs/static/js/game/pong/gameSelectionView.js
@@ -4,19 +4,18 @@ import { updateImageUrl } from "./update.js";
 import { updateSetting } from "./setting.js";
 import { updateSettingB } from "../brickout/update.js";
 
-export function showGameSelectionView()
-{
-	gameVar.liveMatch = false;
-	gameVar.localGame = false;
-	const maincontent = document.getElementById('mainContent');
-	maincontent.innerHTML = '';
-	const gameSelection = document.createElement('div');
+export function showGameSelectionView() {
+  gameVar.liveMatch = false;
+  gameVar.localGame = false;
+  const maincontent = document.getElementById("mainContent");
+  maincontent.innerHTML = "";
+  const gameSelection = document.createElement("div");
 
-	updateImageUrl();
+  updateImageUrl();
 
-	gameSelection.innerHTML =  `
-	<div id="settingView" class="game-selection">
-		<h1>Game Selection</h1>
+  gameSelection.innerHTML = `
+	<div id="settingView" class="d-flex">
+		<h1 class="custom-h1">Game Selection</h1>
 		<div class="container-game">
 			<div class="game-row">
 				<div class="game-title">
@@ -28,7 +27,7 @@ export function showGameSelectionView()
 				<div class="game-settings">
 					<div id="settingsContainer" class="settings-info">
 						<div class="settings-inline">
-							<button id="settingBtn1" class="settingsSelect-button">Settings</button>
+							<button id="settingBtn1" class="main-btn settingsSelect-button">Settings</button>
 							<div class="settings-column" id="settings-column">
 								<p>Difficulty: <span id="difficultyChoice">Medium</span></p>
 								<p>Power-Up: <span id="powerupChoice">❌</span></p>
@@ -38,11 +37,11 @@ export function showGameSelectionView()
 					</div>
 				</div>
 				<div class="game-play">
-					<button id="playBtn" class="startSelect-button">Play</button>
+					<button id="playBtn" class="main-btn settingsSelect-button">Play</button>
 				</div>
 			</div>
 			<div class="game-row">
-				<div class="game-title2">
+				<div class="game-title">
 					<h2 id="gameTitle2">BRICKOUT</h2>
 				</div>
 				<div class="game-image">
@@ -51,7 +50,7 @@ export function showGameSelectionView()
 				<div class="game-settings">
 					<div id="settingsContainer" class="settings-info">
 						<div class="settings-inline">
-							<button id="settingBtn2" class="settingsSelect-button2">Settings</button>
+							<button id="settingBtn2" class="main-btn settingsSelect-button">Settings</button>
 							<div class="settings-column2" id="settings-column2">
 								<p>Difficulty: <span id="difficultyChoice2">Medium</span></p>
 								<p>Power-Up: <span id="powerupChoice2">❌</span></p>
@@ -61,20 +60,21 @@ export function showGameSelectionView()
 					</div>
 				</div>
 				<div class="game-play">
-					<button id="playBtn2" class="startSelect-button">Play</button>
+					<button id="playBtn2" class="main-btn settingsSelect-button">Play</button>
 				</div>
 			</div>
 		</div>
 	</div>`;
 
-	maincontent.appendChild(gameSelection);
-	gameVar.settingBtn1 = document.getElementById('settingBtn1');
-	gameVar.settingBtn2 = document.getElementById('settingBtn2');
-	gameVar.playBtn = document.getElementById('playBtn');
-	gameVar.playBtn2 = document.getElementById('playBtn2');
+  maincontent.appendChild(gameSelection);
+  gameVar.settingBtn1 = document.getElementById("settingBtn1");
+  gameVar.settingBtn2 = document.getElementById("settingBtn2");
+  gameVar.playBtn = document.getElementById("playBtn");
+  gameVar.playBtn2 = document.getElementById("playBtn2");
 
-	updateSetting();
-	updateSettingB();
-	listenSettingBtn();
-	listenPlayBtn();
+  updateSetting();
+  updateSettingB();
+  listenSettingBtn();
+  listenPlayBtn();
 }
+
