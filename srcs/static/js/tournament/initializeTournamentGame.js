d10cfced5fbce71ab1ccd46df25a1fbbf099ad97
lannur-s <lathaannur@gmail.com>
2024-12-09 17:26:21 +0100

    Testing Tournament-Game integration

--- srcs/static/js/tournament/initializeTournamentGame.js
+++ srcs/static/js/tournament/initializeTournamentGame.js
@@ -56,42 +56,6 @@ export function initializeLobbySocket() {
 	};
 }
 
-export function joinRoom(roomName) {
-	const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-	const gameSocket = new WebSocket(protocol + '//' + window.location.host + `/ws/pong/${roomName}/`);
-
-	gameSocket.onopen = function(e) {
-		console.log('Game socket opened');
-		gameVar.gameSocket = gameSocket;
-		try {
-			gameSocket.send(JSON.stringify({ type: 'join_room' }));
-			if (gameVar.playerIdx == 1) {
-				waitingPlayer();
-			}
-			if (gameVar.playerIdx == 2) {
-				sendPlayerData(gameVar.gameSocket, gameVar.playerReady);
-				updateSettingLive();
-			}
-		} catch (error) {
-			console.error("error on send message: ", error);
-		}
-	};
-
-	gameSocket.onmessage = function(e) {
-		const data = JSON.parse(e.data);
-		console.log("Game data: ", data);
-		// Handle game messages
-	};
-
-	gameSocket.onerror = function(e) {
-		console.error('Game socket error:', e);
-	};
-
-	gameSocket.onclose = function(e) {
-		console.error('Game socket closed unexpectedly code : ', e.code, 'reason', e.reason);
-	};
-}
-
 export function initializeGameVariables(difficulty, currentLevel) {
 	gameVar.gameReady = false;
 	gameVar.difficulty = difficulty;
