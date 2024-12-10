8dcaf52b109da0aa8dc8f43bfb50b0ca7efda819
yb <yabingc7@gmail.com>
2024-12-10 13:48:01 +0100

    optimized: add a endpoint for leaderbord view + new match history view

--- srcs/backend/user/urls.py
+++ srcs/backend/user/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
 	path('public/<int:pk>/', views.GetUserPublicInfos, name="user_public_infos"),
 	path('private/', views.GetUserPrivateInfos, name="user_private_infos"),
 	path('private/pk/', views.getUserPk, name="user_pk"),
+	
+	path('leaderbaord/', views.getLeaderboard, name='get_leaderboard'),
 
 	path('register/', views.CreateUserView.as_view(), name="register"),
 	path('activate/<uidb64>/<token>/<action>/', views.ActivateLinkView.as_view(), name="activate_link"),
@@ -20,6 +22,7 @@ urlpatterns = [
 
 	path('friend-requests/<int:pk>/', views.ListFriendRequestView.as_view(), name='list_friend_request'),
 	path('friend-requests/accept/<int:request_id>/', views.AcceptFriendRequestView.as_view(), name='accept_friend_request'),
+
 
 	# authentication endpoints
 	path('check-auth/', views.check_auth, name="check_auth"),
