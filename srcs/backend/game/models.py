c035808990a8530b407fd9250419c3ac32750863
ScrapingArt <scrapingart@protonmail.com>
2024-12-11 09:26:41 +0100

    livechat model in game

--- srcs/backend/game/models.py
+++ srcs/backend/game/models.py
@@ -83,3 +83,11 @@ class GamePlayer(models.Model):
 
 	def __str__(self):
 		return f"GamePlayer {self.user.username} in Game {self.game.id}"
+
+class Message(models.Model):
+	nickname = models.CharField()
+	content = models.TextField()
+	timestamp = models.DateTimeField()
+
+	def __str__(self):
+		return f'{self.nickname}: {self.content}'
