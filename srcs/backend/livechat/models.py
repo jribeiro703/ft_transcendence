c55dc25bd5a52fe77ff31f30fb2417f9e82c5158
ScrapingRat <scrapingrat@protonmail.com>
2024-12-12 19:58:07 +0100

    gamechat msgs, the div is invisible

--- srcs/backend/livechat/models.py
+++ srcs/backend/livechat/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 class Message(models.Model):
-    nickname = models.CharField()
+    nickname = models.CharField(max_length=30)
     content = models.TextField()
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    room = models.CharField(max_length=50, null=True, db_index=True)  # Add indexed room field
+    is_game_chat = models.BooleanField(default=False)  # Flag for game messages
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['room', '-timestamp']),  # Optimize room+timestamp queries
+        ]
 
     def __str__(self):
-        return f'{self.nickname}: {self.content}'
+        room_info = f'(room: {self.room})' if self.room else ''
+        return f'{self.nickname}: {self.content} {room_info}'
