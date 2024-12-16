a7a424714d675689d97cdd8a2feb1e5595924bb6
ScrapingRat <scrapingrat@protonmail.com>
2024-12-16 12:01:17 +0100

    livechat notifs model, isgamepagechat brickout

--- srcs/backend/livechat/models.py
+++ srcs/backend/livechat/models.py
@@ -15,3 +15,22 @@ class Message(models.Model):
     def __str__(self):
         room_info = f'(room: {self.room})' if self.room else ''
         return f'{self.nickname}: {self.content} {room_info}'
+    
+class Notification(models.Model):
+    nickname_sender = models.CharField(max_length=30)
+    nickname_receiver = models.CharField(max_length=30)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    notification_type = models.CharField(max_length=50, choices=[('friend_request', 'Friend Request'), ('game_invite', 'Game Invite'), ('tournament_invite', 'Tournament Invite')])
+    is_read = models.BooleanField(default=False)
+    is_accepted = models.BooleanField(default=False)
+
+    def to_dict(self):
+        return {
+            'content': self.content,
+            'is_read': self.is_read,
+            'is_accepted': self.is_accepted,
+            'notification_type': self.notification_type,
+            'timestamp': self.created_at.isoformat()
+        }
