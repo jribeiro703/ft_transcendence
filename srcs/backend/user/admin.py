c8b4e8faff862f2c08f8b84199b8ce213a4bcee1
ScrapingArt <scrapingart@protonmail.com>
2024-12-17 16:26:42 +0100

    - cleanup (multiple files) - add friend button and request (request not handled yet, must include in notifications later) - add block button and entry in models (blocking not effective yet) - closing gamechat when leaving game - including livechat.html in index.html - include _livechat.css in style.css - loading the emoji font in css to avoid CORS errors - rewrote online/friends models to get more data and faster requests - added friends/blocked in admin

--- srcs/backend/user/admin.py
+++ srcs/backend/user/admin.py
@@ -1,14 +1,47 @@
 from django.contrib import admin
+from django import forms
 from .models import User
 from game.models import Game
 from game.models import GamePlayer
 from tournament.models import Tournament
 
+class UserAdminForm(forms.ModelForm):
+	class Meta:
+		model = User
+		fields = '__all__'
+
+	friends = forms.ModelMultipleChoiceField(
+		queryset=User.objects.all(),
+		required=False,
+		widget=admin.widgets.FilteredSelectMultiple('Friends', is_stacked=False)
+	)
+
+	blocklist = forms.ModelMultipleChoiceField(
+		queryset=User.objects.all(),
+		required=False,
+		widget=admin.widgets.FilteredSelectMultiple('Blocked Users', is_stacked=False)
+	)
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		if self.instance.pk:
+			self.fields['friends'].initial = self.instance.friends.all()
+			self.fields['blocklist'].initial = self.instance.blocklist.all()
+
+	def save(self, commit=True):
+		user = super().save(commit=False)
+		if commit:
+			user.save()
+		if user.pk:
+			user.friends.set(self.cleaned_data['friends'])
+			user.blocklist.set(self.cleaned_data['blocklist'])
+			self.save_m2m()
+		return user
+
 class UserAdmin(admin.ModelAdmin):
-	list_display = ('id', 'username', 'email',)
-	search_fields = ('username', 'alias', 'email',)
-	ordering = ('id',)
-	list_filter = ('email',)
+	form = UserAdminForm
+	list_display = ('username', 'email', 'is_online')
+	search_fields = ('username', 'email')
 
 class GameAdmin(admin.ModelAdmin):
 	ordering = ('created_at',)
