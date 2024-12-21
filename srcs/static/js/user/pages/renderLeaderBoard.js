37e80f6561889dea033937b49bd61cf4bb7d9b0d
dlacuey <lacuey.david@gmail.com>
2024-12-21 23:48:26 +0100

    ajout de details et meilleur mise en page du leaderboard

--- srcs/static/js/user/pages/renderLeaderBoard.js
+++ srcs/static/js/user/pages/renderLeaderBoard.js
@@ -1,10 +1,10 @@
-import { fetchAuthData } from "../fetchData.js"
+import { fetchAuthData } from "../fetchData.js";
 import { showErrorMessages } from "../tools.js";
 
 function createLeaderBoardContent(data) {
-	const mainContent = document.getElementById("mainContent");
-	mainContent.innerHTML = `
-		<div id="leaderboard-container">
+  const mainContent = document.getElementById("mainContent");
+  mainContent.innerHTML = `
+		<div id="leaderboard-container" class="overflow-auto leaderboard justify-content-center">
 			<div>
 				<ul id="leaderboard-list">
 				</ul>
@@ -12,12 +12,12 @@ function createLeaderBoardContent(data) {
 		</div>
 	`;
 
-	const leaderboardList = document.getElementById("leaderboard-list");
+  const leaderboardList = document.getElementById("leaderboard-list");
 
-	data.forEach((user, index) => {
-		const listItem = document.createElement("li");
-		listItem.classList.add("leaderboard-item");
-		listItem.innerHTML = `
+  data.forEach((user, index) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add("leaderboard-item");
+    listItem.innerHTML = `
 			<div class="leaderboard-rank">#${index + 1}</div>
 			<img class="leaderboard-avatar" src="${user.avatar}" alt="${user.username}'s avatar" />
 			<div class="leaderboard-info">
@@ -30,24 +30,29 @@ function createLeaderBoardContent(data) {
 				</p>
 			</div>
 		`;
-		leaderboardList.appendChild(listItem);
-	});
+    leaderboardList.appendChild(listItem);
+  });
 }
 
 function sortLeaderBoardData(leaderBoardArray) {
-	leaderBoardArray.sort((a, b) => b.matchs.won_matches - a.matchs.won_matches);
-	return leaderBoardArray;
+  leaderBoardArray.sort((a, b) => b.matchs.won_matches - a.matchs.won_matches);
+  return leaderBoardArray;
 }
 
 export async function renderLeaderBoardPage() {
+  const responseObject = await fetchAuthData(
+    "/user/leaderboard/",
+    "GET",
+    null,
+    false,
+  );
+  if (responseObject.status != 200) {
+    showErrorMessages(responseObject);
+    return;
+  }
+  const leaderBoardArray = Object.values(responseObject.data);
+  const sortedDataArray = sortLeaderBoardData(leaderBoardArray);
 
-	const responseObject = await fetchAuthData("/user/leaderboard/", "GET", null, false);
-	if (responseObject.status != 200) {
-		showErrorMessages(responseObject);
-		return;
-	}
-	const leaderBoardArray = Object.values(responseObject.data);
-	const sortedDataArray = sortLeaderBoardData(leaderBoardArray);
-
-	createLeaderBoardContent(sortedDataArray);
+  createLeaderBoardContent(sortedDataArray);
 }
+
--- srcs/static/js/user/pages/renderProfilePage.js
+++ srcs/static/js/user/pages/renderProfilePage.js
@@ -1,4 +1,4 @@
-import { showToast, DEFAULT_AVATAR} from "../tools.js";
+import { showToast, DEFAULT_AVATAR } from "../tools.js";
 import { fetchData, fetchAuthData } from "../fetchData.js";
 import { renderPage } from "../historyManager.js";
 
@@ -8,8 +8,8 @@ let matchData;
 let userData;
 
 function createProfileContent() {
-	const mainContent = document.getElementById('mainContent');
-	mainContent.innerHTML = `
+  const mainContent = document.getElementById("mainContent");
+  mainContent.innerHTML = `
 		<div class="profile-container" >
     <div class="profile-scrollable">
 			<div class="profile-header">
@@ -43,53 +43,58 @@ function createProfileContent() {
 }
 
 function updateNavigationButtons(totalPages) {
-	const prevButton = document.getElementById('prevButton');
-	const nextButton = document.getElementById('nextButton');
+  const prevButton = document.getElementById("prevButton");
+  const nextButton = document.getElementById("nextButton");
 
-	prevButton.disabled = currentPage === 1;
-	nextButton.disabled = currentPage === totalPages;
+  prevButton.disabled = currentPage === 1;
+  nextButton.disabled = currentPage === totalPages;
 
-	prevButton.onclick = () => {
-		if (currentPage > 1) {
-			currentPage--;
-			createMatchHistory(document.getElementById('matchHistory'), { match_history: matchData, ...userData });
-		}
-	};
+  prevButton.onclick = () => {
+    if (currentPage > 1) {
+      currentPage--;
+      createMatchHistory(document.getElementById("matchHistory"), {
+        match_history: matchData,
+        ...userData,
+      });
+    }
+  };
 
-	nextButton.onclick = () => {
-		if (currentPage < totalPages) {
-			currentPage++;
-			createMatchHistory(document.getElementById('matchHistory'), { match_history: matchData, ...userData });
-		}
-	};
+  nextButton.onclick = () => {
+    if (currentPage < totalPages) {
+      currentPage++;
+      createMatchHistory(document.getElementById("matchHistory"), {
+        match_history: matchData,
+        ...userData,
+      });
+    }
+  };
 }
 
 function createMatchHistory(container, data) {
-	matchData = data.match_history;
+  matchData = data.match_history;
 
-// console.log(matchData);
+  // console.log(matchData);
 
-	userData = data;
+  userData = data;
 
-// console.log(userData);
+  // console.log(userData);
 
-	const totalMatches = matchData.length;
-	const totalPages = Math.ceil(totalMatches / matchesPerPage);
-	const startIndex = (currentPage - 1) * matchesPerPage;
-	const endIndex = startIndex + matchesPerPage;
-	const paginatedMatches = matchData.slice(startIndex, endIndex);
+  const totalMatches = matchData.length;
+  const totalPages = Math.ceil(totalMatches / matchesPerPage);
+  const startIndex = (currentPage - 1) * matchesPerPage;
+  const endIndex = startIndex + matchesPerPage;
+  const paginatedMatches = matchData.slice(startIndex, endIndex);
 
-	if (paginatedMatches.length > 0) {
-		const table = document.createElement('table');
-		table.classList.add('match-history-table');
-		paginatedMatches.forEach(match => {
+  if (paginatedMatches.length > 0) {
+    const table = document.createElement("table");
+    table.classList.add("match-history-table");
+    paginatedMatches.forEach((match) => {
+      // console.log(match);
 
-			// console.log(match);
-
-			const row = document.createElement('tr');
-			row.classList.add('match-row');
-			row.innerHTML = `
-				<td class="match-result">${match.winner === userData.username ? 'WIN' : 'LOSS'}</td>
+      const row = document.createElement("tr");
+      row.classList.add("match-row");
+      row.innerHTML = `
+				<td class="match-result">${match.winner === userData.username ? "WIN" : "LOSS"}</td>
 				<td class="user-info">
 					<div class="user-name" >${userData.username}</div>
 					<img id="user-avatar" class="user-avatar" src="${userData.avatar}" alt="${userData.username}'s avatar"/>
@@ -103,69 +108,72 @@ function createMatchHistory(container, data) {
 				</td>
 				<td class="match-date">${match.date}</td>
 			`;
-			table.appendChild(row);
-		});
-		
-		container.innerHTML = '';
-		container.appendChild(table);
-		updateNavigationButtons(totalPages);
-	
+      table.appendChild(row);
+    });
 
-		const userAvatars = document.querySelectorAll('.user-avatar');
-		userAvatars.forEach(avatar => {
-			avatar.style.cursor = 'pointer';
-			avatar.addEventListener('click', () => {
-				console.log("userData username: ", userData.username);
-				renderPage("profile", false, userData.username);
-			});
-		});
+    container.innerHTML = "";
+    container.appendChild(table);
+    updateNavigationButtons(totalPages);
 
-		const enemyAvatars = document.querySelectorAll('.enemy-avatar');
-		enemyAvatars.forEach(avatar => {
-			avatar.style.cursor = 'pointer';
-			avatar.addEventListener('click', () => {
-				const enemyUsername = avatar.alt.replace("'s avatar", '');
-				console.log("enemy username: ", enemyUsername);
-				renderPage("profile", true, enemyUsername);
-			});
-		});
+    const userAvatars = document.querySelectorAll(".user-avatar");
+    userAvatars.forEach((avatar) => {
+      avatar.style.cursor = "pointer";
+      avatar.addEventListener("click", () => {
+        console.log("userData username: ", userData.username);
+        renderPage("profile", false, userData.username);
+      });
+    });
 
-	} else {
-		container.innerHTML = '<p>No match history available.</p>';
-	}
+    const enemyAvatars = document.querySelectorAll(".enemy-avatar");
+    enemyAvatars.forEach((avatar) => {
+      avatar.style.cursor = "pointer";
+      avatar.addEventListener("click", () => {
+        const enemyUsername = avatar.alt.replace("'s avatar", "");
+        console.log("enemy username: ", enemyUsername);
+        renderPage("profile", true, enemyUsername);
+      });
+    });
+  } else {
+    container.innerHTML = "<p>No match history available.</p>";
+  }
 }
 
 export async function renderProfilePage(username) {
+  // console.log("renderProfilePage: ", username);
 
-	// console.log("renderProfilePage: ", username);
+  const responseObject = await fetchAuthData(
+    `/user/profile/${username}/`,
+    "GET",
+    null,
+    false,
+  );
+  if (responseObject.status === 200) {
+    createProfileContent();
+    const data = responseObject.data;
 
-	const responseObject = await fetchAuthData(`/user/profile/${username}/`, "GET", null, false);
-	if (responseObject.status === 200) {
-		createProfileContent();
-		const data = responseObject.data;
+    const statusIndicator = document.querySelector(
+      "#isOnline .status-indicator",
+    );
+    const statusText = document.querySelector("#isOnline .status-text");
 
-		const statusIndicator = document.querySelector('#isOnline .status-indicator');
-		const statusText = document.querySelector('#isOnline .status-text');
-		
-		if (data.is_online) {
-			statusIndicator.classList.add('status-online');
-			statusText.textContent = 'Online';
-		} else {
-			statusIndicator.classList.add('status-offline');
-			statusText.textContent = 'Offline';
-		}
+    if (data.is_online) {
+      statusIndicator.classList.add("status-online");
+      statusText.textContent = "Online";
+    } else {
+      statusIndicator.classList.add("status-offline");
+      statusText.textContent = "Offline";
+    }
 
-		document.getElementById('username').textContent = data.username;
-		document.getElementById('avatar').src = data.avatar;
-		if (data.alias)
-			document.getElementById('alias').textContent = `(${data.alias})`;
-		document.getElementById('totalMatches').textContent = data.total_matches;
-		document.getElementById('wonMatches').textContent = data.won_matches;
-		
-		const matchHistory = document.getElementById('matchHistory');
-		createMatchHistory(matchHistory, data);
+    document.getElementById("username").textContent = data.username;
+    document.getElementById("avatar").src = data.avatar;
+    if (data.alias)
+      document.getElementById("alias").textContent = `(${data.alias})`;
+    document.getElementById("totalMatches").textContent = data.total_matches;
+    document.getElementById("wonMatches").textContent = data.won_matches;
 
-	} else {
-		showToast("An error occurred while fetching profile data.", "error");
-	}
+    const matchHistory = document.getElementById("matchHistory");
+    createMatchHistory(matchHistory, data);
+  } else {
+    showToast("An error occurred while fetching profile data.", "error");
+  }
 }
