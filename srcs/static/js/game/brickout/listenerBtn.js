a6abe4a4933e6890dcb17d571d62762fc34daab0
botyonthesky <maillard.thibaud@gmail.com>
2025-01-09 13:21:37 +0100

    frontGameFix: add class and fix bug on restartLevelBtn and nextLevelBtn

--- srcs/static/js/game/brickout/listenerBtn.js
+++ srcs/static/js/game/brickout/listenerBtn.js
@@ -11,11 +11,27 @@ export function listenNextLevelBtn()
 	const quitBtn = document.getElementById("quitBtn");
 	const restartLevelBtn = document.getElementById("restartLevelBtn")
 	if (nextLevelBtn)
-		nextLevelBtn.addEventListener("click", handleNextLevelB);
+	{
+		nextLevelBtn.addEventListener("click", () =>
+		{
+			handleNextLevelB();
+		});
+	}
+
 	if (restartLevelBtn)
-		restartLevelBtn.addEventListener("click", restartLevelB);
+	{
+		restartLevelBtn.addEventListener("click", () =>
+		{
+			restartLevelB();
+		});
+	}
 	if (quitBtn)
-		quitBtn.addEventListener('click', () => renderPageGame("home", true));
+	{
+		quitBtn.addEventListener('click', () => 
+		{
+			renderPageGame("home", true);
+		});
+	}
 }
 
 export function listenFinishBtn()
