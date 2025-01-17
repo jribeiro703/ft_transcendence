575d7c5252c95e58d68828f1c819bdc0936a212e
dlacuey <lacuey.david@gmail.com>
2025-01-17 13:38:29 +0100

    ajout de la navbar en bas

--- srcs/static/js/livechat/utils.js
+++ srcs/static/js/livechat/utils.js
@@ -3,14 +3,14 @@ import { fetchAuthData } from '../user/fetchData.js';
 export const clientIdColors = {};
 
 export const chatIconDots = `
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-dots-fill chat-icon-dots" viewBox="0 0 16 16">
-	<path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm5 4a1 1 0 1 0-2 0 1 1 0 0 0 2 0m4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-chat-left-dots-fill chat-icon-dots" viewBox="0 0 40 40">
+    <path d="M0,5a5,5,0,0,1,5-5H35a5,5,0,0,1,5,5V25a5,5,0,0,1-5,5H11.035a2.5,2.5,0,0,0-1.767.733L2.135,37.865A1.25,1.25,0,0,1,0,36.983ZM12.5,15a2.5,2.5,0,1,0-5,0,2.5,2.5,0,0,0,5,0Zm10,0a2.5,2.5,0,1,0-5,0,2.5,2.5,0,0,0,5,0Zm7.5,2.5a2.5,2.5,0,1,0,0-5,2.5,2.5,0,0,0,0,5"/>
   </svg>
 `;
 
 export const chatIconFill = `
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-fill chat-icon-fill" viewBox="0 0 16 16">
-	<path d="M2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-chat-left-fill chat-icon-fill" viewBox="0 0 40 40">
+    <path d="M5,0A5,5,0,0,0,0,5V36.983a1.25,1.25,0,0,0,2.135.883l7.133-7.133A2.5,2.5,0,0,1,11.035,30H35a5,5,0,0,0,5-5V5a5,5,0,0,0-5-5Z"/>
   </svg>
 `;
 
