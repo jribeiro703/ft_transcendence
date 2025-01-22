04bb851b714dd59e094f1d5ea5f8681627ee96db
lannur-s <lathaannur@gmail.com>
2025-01-22 16:45:00 +0100

    A lot changed to CreateTournament and to display TournamentBracket

--- srcs/backend/tournament/urls.py
+++ srcs/backend/tournament/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 from . import views
 
+from .views.views_tournament import CreateTournamentView, TournamentDetailView
+
+
 urlpatterns = [
-    path("next/<uuid:tournament_id>/", views.go_to_tournament_next_match, name="tournament-next-match"),
-    path("create/", views.create_tournament, name="tournament-create"),
-    path("delete/<uuid:tournament_id>/", views.delete_tournament, name="tournament-delete"),
-    path("info/", views.get_tournament_info, name="tournament-info"),
+    path("create/", CreateTournamentView.as_view(), name='create-tournament'),
+    path("<int:tournament_id>/", TournamentDetailView.as_view(), name="tournament-detail"),
+    path("next/<int:tournament_id>/", views.go_to_tournament_next_match, name="tournament-next-match"),
+    path("delete/<int:tournament_id>/", views.delete_tournament, name="tournament-delete"),
     path("list/", views.tournament_list, name="tournament-list"),
     path("tokenvalidate", views.validate_game_token, name="validate-game-token"),
 ]
--- srcs/backend/tournament/views/views_tournament.py
+++ srcs/backend/tournament/views/views_tournament.py
@@ -1,222 +1,272 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 from django.db import models
-from user.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.translation import gettext as _
 from django.db.models import Q
 from django.http import JsonResponse
-from tournament.models import Tournament, TournamentMatch, Guest
-from game.models import Game
+
 import json
 import random
 
-#used
-@csrf_exempt
-@login_required
-def create_tournament(request):
-	if request.method == 'POST':
-		logger.info(f"Request user: {request.user}")
-		request_user = request.user
-		# Fixed number of players and matches for a simplified tournament
-		nb_players = 4
-		nb_matches = 3
-		difficulty = "EASY"  # Default difficulty
+from django.apps import apps
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
+from tournament.views.utils import get_user_role
 
-		players = []
-		for i in range(1, nb_players + 1):
-			user_key = f'player{i}_user'
-			guest_key = f'player{i}_guest'
-			user_id = request.POST.get(user_key)
-			guest_name = request.POST.get(guest_key)
+from user.models import User
+from tournament.models import Tournament, Guest, TournamentMatch
+from game.models import Game
 
-			if user_id:
-				try:
-					user = User.objects.get(id=user_id)
-					players.append((user, None))
-				except User.DoesNotExist:
-					return JsonResponse({'error': f'User with id {user_id} does not exist'}, status=400)
-			elif guest_name:
-				guest, created = Guest.objects.get_or_create(display_name=guest_name)
-				players.append((None, guest))
-			else:
-				#break
-				return JsonResponse({'error': 'Invalid players. Must provide exactly 4 players.'}, status=400)
+import logging
 
-		if len(players) != nb_players:
-			return JsonResponse({'error': 'Invalid number of players. Must be exactly 4.'}, status=400)
-		
-		# Shuffle the players
-		random.shuffle(players)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-		# Create the Tournament
-		tournament = Tournament(
-			created_by=request_user,
-			status="open",
-			player_count=nb_players,
-			current_match=1
-		)
-		tournament.save()
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_201_CREATED
+from django.db import transaction
+from tournament.models import Tournament, TournamentMatch, Guest
+from game.models import Game
+from user.models import User
+import random
+import logging
 
-		# Get all registered users from players list
-		registered_users = [player[0] for player in players if player[0]]
-		for user in registered_users:
-			tournament.registered_users.add(user)
-			tournament.save()
+logger = logging.getLogger(__name__)
 
+class CreateTournamentView(APIView):
+	permission_classes = [IsAuthenticated]
 
-		# Create the 2 initial matches
-		initial_matches = nb_players // 2 # in case of 4 players, 2 initial matches
-		for i in range(2):
-			player1, guest1 = players[2 * i]
-			player2, guest2 = players[2 * i + 1]
+	def post(self, request):
+		try:
+			logger.info(f"Request user: {request.user}, Authenticated: {request.user.is_authenticated}")
+			players = request.data.get('players', [])
+			logger.info(f"Players: {players}")
 
-			game = Game(
-				difficulty=difficulty,
-				tournament=tournament,
-				player_one=player1,
-                player1_guest=guest1,
-                player_two=player2,
-                player2_guest=guest2,
-                score_one=0,
-                score_two=0,
-			)
-			game.save()
+			if len(players) != 4:
+				return Response({'error': 'Invalid number of players. Must provide exactly 4 players.'}, status=HTTP_400_BAD_REQUEST)
 
-			tournament_match = TournamentMatch(
-				tournament=tournament,
-				game=game,
-				position=i + 1,
-				player1_name=game.get_player1_name(),
-                player2_name=game.get_player2_name(),
-			)
-			tournament_match.save()
+			difficulty = "EASY"  # Default difficulty
 
-		# Create subsequent matches without players
-		# this loop is unnecessary, as the number of matches is fixed, this will always be 1, the final match
-		for i in range(initial_matches, nb_matches): 
-			game = Game(
-				difficulty=difficulty,
-				tournament=tournament,
-				score_one=0,
-            	score_two=0,
-			)
-			game.save()
+			player_objects = []
+			for player in players:
+				user_id = player.get('user_id')
+				guest_name = player.get('guest_name')
 
-			tournament_match = TournamentMatch(
-				tournament=tournament,
-				game=game,
-				position=i + 1, # this will always be 3, the final match
-				player1_name="TBD",
-            	player2_name="TBD",
-			)
-			tournament_match.save()
+				if user_id:
+					try:
+						user = User.objects.get(id=user_id)
+						player_objects.append((user, None))
+					except User.DoesNotExist:
+						return Response({'error': f'User with id {user_id} does not exist.'}, status=HTTP_400_BAD_REQUEST)
+				elif guest_name:
+					guest, created = Guest.objects.get_or_create(display_name=guest_name)
+					player_objects.append((None, guest))
+				else:
+					return Response({'error': 'Invalid player entry. Each player must have either a user_id or guest_name.'}, status=HTTP_400_BAD_REQUEST)
 
-		return JsonResponse({'success': 'Tournament created', 'tournament_id': tournament.id}, status=200)
-	return JsonResponse({'error': 'Invalid request'}, status=400)
+			random.shuffle(player_objects)
 
+			with transaction.atomic():
+				# Create Tournament
+				tournament = Tournament.objects.create(
+					created_by=request.user,
+					status="open",
+					player_count=len(player_objects),
+					current_match=1
+				)
 
-@csrf_exempt
-@login_required
-def get_tournament_info(request):
-	if request.method == 'POST':
-		tournament_id = request.POST.get('tournamentId')
-		tournament = Tournament.objects.get(id=tournament_id)
+				# Register users
+				registered_users = [player[0] for player in player_objects if player[0]]
+				for user in registered_users:
+					tournament.registered_users.add(user)
 
-		matches = TournamentMatch.objects.filter(tournament=tournament).order_by('position')
-		matches_info = []
-		for match in matches:
-			match_info = {
-				'match_id': match.match.id,
-				'position': match.position,
-				'player1': match.player1_name,
-				'player2': match.player2_name,
-				'score_player1': match.match.score_player1,
-				'score_player2': match.match.score_player2
+				first_game_id = None
+				# Create Matches
+				for i in range(2):  # First two matches
+					player1, guest1 = player_objects[2 * i]
+					player2, guest2 = player_objects[2 * i + 1]
+
+					game = Game.objects.create(
+						difficulty=difficulty,
+						tournament=tournament,
+						player_one=player1,
+						player1_guest=guest1,
+						player_two=player2,
+						player2_guest=guest2,
+						score_one=0,
+						score_two=0,
+					)
+					game.save()
+					logger.info(f"Created Game with id: {game.id}")
+
+					if first_game_id is None:
+						first_game_id = game.id  # Capture the first game's ID
+
+					TournamentMatch.objects.create(
+						tournament=tournament,
+						match=game,
+						position=i + 1,
+						player1_name=game.get_player1_name(),
+						player2_name=game.get_player2_name(),
+					)
+					tournament.save()
+					tournament.current_match = first_game_id
+
+				# Create Final Match
+				final_game = Game.objects.create(
+					difficulty=difficulty,
+					tournament=tournament,
+					score_one=0,
+					score_two=0,
+				)
+				final_game.save()
+
+				TournamentMatch.objects.create(
+					tournament=tournament,
+					match=final_game,
+					position=3,
+					player1_name="TBD",
+					player2_name="TBD",
+				)
+				tournament.save()
+
+			return Response({'success': 'Tournament created', 'tournament_id': tournament.id}, status=HTTP_201_CREATED)
+
+		except Exception as e:
+			logger.error(f"Error creating tournament: {str(e)}")
+			return Response({'error': 'An error occurred while creating the tournament.'}, status=HTTP_400_BAD_REQUEST)
+
+class TournamentDetailView(APIView):
+	permission_classes = [IsAuthenticated]
+
+	def get(self, request, tournament_id):
+		logger.info("TournamentDetailView GET method called")
+
+		logger.info(f"Received tournament_id: {tournament_id}")
+		try:
+			tournament = Tournament.objects.get(id=tournament_id)
+			logger.info(f"Tournament details: {tournament}")
+			matches = TournamentMatch.objects.filter(tournament=tournament).order_by('position')
+			matches_info = []
+
+			for match in matches:
+				matches_info.append({
+					'match_id': match.match.id,
+					'position': match.position,
+					'player1': match.player1_name,
+					'player2': match.player2_name,
+					'score_one': match.match.score_one,
+					'score_two': match.match.score_two,
+				})
+
+			data = {
+				"id": tournament.id,
+				"created_by": tournament.created_by.username,
+				"status": tournament.status,
+				"player_count": tournament.player_count,
+				"current_match": tournament.current_match,
+				"matches": matches_info,				
 			}
-			matches_info.append(match_info)
+			return Response(data, status=200)
+		except Tournament.DoesNotExist:
+			return Response({"error": "Tournament not found"}, status=404)
 
-		current_match = matches.filter(position=tournament.current_match).first()
-		if current_match:
-			current_match = {
-				'match_id': current_match.match.id,
-				'position': current_match.position,
-				'player1': current_match.player1_name,
-				'player2': current_match.player2_name,
-				'score_player1': current_match.match.score_player1,
-				'score_player2': current_match.match.score_player2
-			}
-
-		registered_users = tournament.registered_users.all()
-
-		return JsonResponse({
-			'tournament_id': tournament.id,
-			'status': tournament.status,
-			'player_count': tournament.player_count,
-			'current_match': current_match,
-			'winner': tournament.winner,
-			'matches': matches_info,
-			'registered_users': [user.display_name for user in registered_users]
-		}, status=200)
-	return JsonResponse({'error': 'Invalid request'}, status=400)
-
+		except Tournament.DoesNotExist:
+			return Response({"error": "Tournament not found"}, status=404)
 
 @csrf_exempt
-@login_required
 def go_to_tournament_next_match(request, tournament_id):
 	if request.method == 'POST':
-		tournament = Tournament.objects.get(id=tournament_id)
-		max_nb_matches = 3 if tournament.player_count == 4 else 7
+		logger.info(f"Received tournament_id: {tournament_id}")
+		logger.info(f"Raw POST body: {request.body}")
+		max_nb_matches = 3
+		# Parse JSON data from the request body
+		data = json.loads(request.body.decode('utf-8'))
+		logger.info(f"Received JSON data: {data}")
+		try:
+			match_id = data.get('matchId')
+			logger.info(f"Match ID: {match_id}")
+		except json.JSONDecodeError as e:
+			logger.error(f"Failed to parse JSON: {e}")
+			return JsonResponse({'error': 'Invalid JSON payload'}, status=400)
 
-		match_id = request.POST.get('matchId')
-		match = Match.objects.get(id=match_id)
-		match.score_player1 = request.POST.get('score_player1')
-		match.score_player2 = request.POST.get('score_player2')
-		match.status = 'finished'
-		match.save()
+		if not match_id:
+			logger.error("No match ID provided in the request.")
+			return JsonResponse({'error': 'Match ID is required'}, status=400)
+		score_one = data.get('score_one')
+		score_two = data.get('score_two')
+		try:
+			tournament = Tournament.objects.get(id=tournament_id)
+			logger.info(f"Received match_id: {match_id}")
+			try:
+				match = Game.objects.get(id=match_id)
+			except Game.DoesNotExist:
+				logger.error(f"Game with id {match_id} does not exist.")
+				return JsonResponse({'error': f'Game with id {match_id} not found'}, status=404)
+			
+			if score_one is None or score_two is None:
+				logger.error("Scores are required to update the match.")
+				return JsonResponse({'error': 'Scores are required'}, status=400)
 
-		# Get the winner User of the match
-		if match.score_player1 > match.score_player2:
-			winner_user = match.player1_user
-			winner_guest = match.player1_guest
-			winner = match.player1_user.display_name if match.player1_user else match.player1_guest.display_name
-		else:
-			winner_user = match.player2_user
-			winner_guest = match.player2_guest
-			winner = match.player2_user.display_name if match.player2_user else match.player2_guest.display_name
+			match.score_one = score_one
+			match.score_two = score_two
+			match.status = 'finished'
+			match.save()
 
-		# Get all tournament matches
-		tournamentMatches = TournamentMatch.objects.filter(tournament=tournament).order_by('position')
-		next_match = None
-		for tournamentMatch in tournamentMatches:
-			if tournamentMatch.player1_name == "TBD":
-				tournamentMatch.player1_name = winner
-				tournamentMatch.save()
-				next_match = Match.objects.get(id=tournamentMatch.match.id)
-				next_match.player1_user = winner_user
-				next_match.player1_guest = winner_guest
-				next_match.save()
-				break
-			if tournamentMatch.player2_name == "TBD":
-				tournamentMatch.player2_name = winner
-				tournamentMatch.save()
-				next_match = Match.objects.get(id=tournamentMatch.match.id)
-				next_match.player2_user = winner_user
-				next_match.player2_guest = winner_guest
-				next_match.save()
-				break
+			# Get the winner User of the match
+			if match.score_one > match.score_two:
+				winner_user = match.player_one
+				winner_guest = match.player1_guest
+				winner = match.player_one.display_name if match.player_one else match.player1_guest.display_name
+			else:
+				winner_user = match.player_two
+				winner_guest = match.player2_guest
+				winner = match.player_two.display_name if match.player_two else match.player2_guest.display_name
 
-		if not next_match:
-			tournament.status = 'finished'
-			tournament.winner = winner
-			tournament.current_match = 0
-			tournament.save()
-			return JsonResponse({'success': 'Tournament finished'}, status=200)
-		else:
-			tournament.current_match += 1
-			tournament.save()
-			return JsonResponse({'success': 'Next match'}, status=200)
-	return JsonResponse({'error': 'Invalid request'}, status=400)
+			# Get all tournament matches
+			tournamentMatches = TournamentMatch.objects.filter(tournament=tournament).order_by('position')
+			next_match = None
+			for tournamentMatch in tournamentMatches:
+				if tournamentMatch.player1_name == "TBD":
+					tournamentMatch.player1_name = winner
+					tournamentMatch.save()
+					next_match = Game.objects.get(id=tournamentMatch.match.id)
+					next_match.player_one = winner_user
+					next_match.player1_guest = winner_guest
+					next_match.save()
+					break
+				if tournamentMatch.player2_name == "TBD":
+					tournamentMatch.player2_name = winner
+					tournamentMatch.save()
+					next_match = Game.objects.get(id=tournamentMatch.match.id)
+					next_match.player_two = winner_user
+					next_match.player2_guest = winner_guest
+					next_match.save()
+					break
 
+			if not next_match:
+				tournament.status = 'finished'
+				tournament.winner = winner
+				tournament.current_match = 0
+				tournament.save()
+				return JsonResponse({'success': 'Tournament finished'}, status=200)
+			else:
+				tournament.current_match += 1
+				tournament.save()
+				return JsonResponse({'success': 'Next match launched'}, status=200)
+		except Tournament.DoesNotExist:
+			return JsonResponse({'error': 'Tournament not found'}, status=404)
+		except Exception as e:
+			logger.error(f"Error in go_to_tournament_next_match: {e}")
+			return JsonResponse({'error': 'Failed to launch next match'}, status=500)
+	return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 @login_required
 def tournament_list(request):
