7952ca13b73b7d25b98b86ab6abc67823cf0a1ff
dlacuey <lacuey.david@gmail.com>
2025-01-27 09:32:53 +0100

    ok

--- srcs/static/js/user/pages/renderAuthPage.js
+++ srcs/static/js/user/pages/renderAuthPage.js
@@ -7,7 +7,7 @@ function createAuthContent() {
   box.innerHTML = `
 			<div id="defaultView"
 				 class="d-flex flex-column justify-content-center align-items-center gap-4 h-100 w-100">
-			  <img class="img-fluid neon-white main-img" src="${PONG_CARD}" alt="Pong Game">
+			  <img class="img-fluid neon-main-image main-img" src="${PONG_CARD}" alt="Pong Game">
 			<button id="btn-Login" class="primaryBtn w-380"><span>Login</span></button>
 			<button id="btn-Register" class="primaryBtn w-380"><span>Register</span></button>
 			<button id="btn-Login42" class="primaryBtn w-380"><span>42 Login</span></button>
